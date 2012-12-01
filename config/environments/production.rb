f35e0c18cfc99fc0e4fd955419a57ada8e562d22
Katarina <katarina@katarina-VirtualBox.(none)>
2012-12-01 20:50:17 +0100

    config.assets.precompile customized

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ DemoApp::Application.configure do
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true                #change just to check if it works
+  config.assets.compress = false
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
@@ -46,7 +46,7 @@ DemoApp::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += Dir.glob( File.join("**/blueprint/*.css") )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
